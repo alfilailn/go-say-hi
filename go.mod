48ed64c4916ffdf5b0a7c0bdac03f0e778eef3e9
alfilailn <nur.alfilail98@gmail.com>
2022-10-15 21:38:35 +0700

    new v3

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/alfilailn/go-say-hi/v2
+module github.com/alfilailn/go-say-hi/v3
 
 go 1.18
--- say_hi.go
+++ say_hi.go
@@ -1,5 +1,5 @@
 package gosayhi
 
-func SayHi(name string) string {
-	return "Hello " + name
+func SayHi(time, name string) string {
+	return "Hello " + name + ". Good " + time
 }
